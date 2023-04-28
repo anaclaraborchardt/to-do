706592ae659c80faf5a4c77653995a560e33f367
anaclaraborchardt <anaclaratomaselliborchardt75@gmail.com>
2023-04-27 21:55:40 -0300

    alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Pessoa{
+
+  tarefa: string;
+  categoria: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  
+  mostraInput: boolean =true
+  
+  usuarios: Pessoa []=[]
+
+
+  tarefa:string
+  
+
+  pessoa: Pessoa = {
+    tarefa:'',
+    categoria:''
+  }
+
+inputMudou(event):void{
+  console.log(event)
+}
+
+mostrarInput(): void{
+  this.mostraInput=true;
+}
+
+esconderInput(): void{
+  this.mostraInput=false;
+
+}
+cadastrarusuario(): void {
+  const usuario: Pessoa = {
+    tarefa: this.pessoa.tarefa,
+    categoria: this.pessoa.categoria
+  }
+
+  this.usuarios.push(usuario);
+  localStorage.setItem('usuarios', JSON.stringify(this.usuarios));
+
+  this.tarefa = '';
+}
+
+removerUsuario():void{
+localStorage.removeItem('usuario');
+}
+   
 }
