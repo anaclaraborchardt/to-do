97ccdfb6e6106343d85e6e8f34169ab25d9e9618
anaclaraborchardt <130708738+anaclaraborchardt@users.noreply.github.com>
2023-05-03 15:48:19 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,44 +15,60 @@ export class AppComponent {
   title = 'todo-app';
   
   mostraInput: boolean =true
-  
   usuarios: Pessoa []=[]
-
-
   tarefa:string
-  
+  novaCategoria: string;
+  tarefaSelecionada: Pessoa = null;
 
   pessoa: Pessoa = {
     tarefa:'',
-    categoria:''
+    categoria:'',
   }
 
-inputMudou(event):void{
-  console.log(event)
+cadastrarusuario(): void {
+  const usuario: Pessoa = {
+    tarefa: this.pessoa.tarefa,
+    categoria:this.pessoa.categoria
+  };
+  
+
+  this.usuarios.push(usuario);
+  this.limparForm();
+  localStorage.setItem('usuarios', JSON.stringify(this.usuarios));
+
+  this.pessoa = {
+    tarefa:'',
+    categoria:'',
+  };
+}
+ngOnInit() {
+  const usuario = localStorage.getItem('usuarios');
+  if (usuario) {
+    this.usuarios = JSON.parse(usuario);
+  }
+}
+
+removerUsuario(usuario: Pessoa): void {
+  const indice = this.usuarios.indexOf(usuario);
+  this.usuarios.splice(indice, 1);
+  localStorage.setItem('usuarios', JSON.stringify(this.usuarios));
 }
 
 mostrarInput(): void{
   this.mostraInput=true;
 }
 
-esconderInput(): void{
-  this.mostraInput=false;
+limparForm(): void{
+  this.pessoa.tarefa = '';
+  this.pessoa.categoria = '';
 
 }
-cadastrarusuario(): void {
-  const usuario: Pessoa = {
-    tarefa: this.pessoa.tarefa,
-    categoria: this.pessoa.categoria
+
+atualizarTarefa(usuario: Pessoa) {
+  if (usuario.categoria) {
+    usuario.categoria = usuario.categoria;
   }
-
-  this.usuarios.push(usuario);
-  localStorage.setItem('usuarios', JSON.stringify(this.usuarios));
-
-  this.tarefa = '';
-}
-
-removerUsuario():void{
-localStorage.removeItem('usuario');
+  this.limparForm();
 }
    
 }
