463ab0fa6fe9bf1fd3309b51c18dac6d96b62fea
anaclaraborchardt <anaclaratomaselliborchardt75@gmail.com>
2023-06-05 19:25:53 -0300

    dragdrop

--- src/tarefa/tarefa.component.ts
+++ src/tarefa/tarefa.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
-interface Pessoa{
-
+interface Pessoa {
   tarefa: string;
-  categoria: string
+  categoria: string;
 }
 
 @Component({
@@ -12,35 +11,37 @@ interface Pessoa{
   styleUrls: ['./tarefa.component.css']
 })
 export class TarefaComponent {
-    mostraInput: boolean =true
-    usuarios: Pessoa []=[]
-    tarefa:string
-    novaCategoria: string;
-    tarefaSelecionada: Pessoa = null;
-    categorias: string[] = [];
-    tarefasFiltradas: Pessoa[] = [];
-  
-    pessoa: Pessoa = {
-      tarefa:'',
-      categoria:'',
-    }
-  
+  mostraInput: boolean = true;
+  usuarios: Pessoa[] = [];
+  tarefa: string;
+  novaCategoria: string;
+  tarefaSelecionada: Pessoa = null;
+  categorias: string[] = [];
+  tarefasFiltradas: Pessoa[] = [];
+  tarefaDrop: Pessoa;
+  categoriaDrop: string;
+
+  pessoa: Pessoa = {
+    tarefa: '',
+    categoria: '',
+  };
+
   cadastrarTarefa(): void {
     const usuario: Pessoa = {
       tarefa: this.pessoa.tarefa,
-      categoria:this.pessoa.categoria
+      categoria: this.pessoa.categoria,
     };
-    
-  
+
     this.usuarios.push(usuario);
     this.limparForm();
     localStorage.setItem('usuarios', JSON.stringify(this.usuarios));
-  
+
     this.pessoa = {
-      tarefa:'',
-      categoria:'',
+      tarefa: '',
+      categoria: '',
     };
   }
+
   ngOnInit() {
     const usuario = localStorage.getItem('usuarios');
     if (usuario) {
@@ -51,26 +52,47 @@ export class TarefaComponent {
       this.categorias = JSON.parse(categoriasSalvas);
     }
   }
-  
+
   removerUsuario(usuario: Pessoa): void {
     const indice = this.usuarios.indexOf(usuario);
     this.usuarios.splice(indice, 1);
     localStorage.setItem('usuarios', JSON.stringify(this.usuarios));
   }
-  
-  mostrarInput(): void{
-    this.mostraInput=true;
+
+  mostrarInput(): void {
+    this.mostraInput = true;
   }
-  
-  limparForm(): void{
+
+  limparForm(): void {
     this.pessoa.tarefa = '';
     this.pessoa.categoria = '';
-  
   }
-  
+
   atualizarTarefa(usuario: Pessoa, novaCategoria: string) {
     usuario.categoria = novaCategoria;
     localStorage.setItem('usuarios', JSON.stringify(this.usuarios));
-  }   
+  }
 
+  executaDrop(event: DragEvent, categoria: string): void {
+    event.preventDefault();
+  }
+
+  drop(event: DragEvent, categoria: string): void {
+    event.preventDefault();
+
+    const tarefaArrastada = event.dataTransfer.getData('text/plain');
+    const tarefa = this.usuarios.find((usuario) => usuario.tarefa === tarefaArrastada);
+    if (tarefa) {
+      tarefa.categoria = categoria;
+      localStorage.setItem('usuarios', JSON.stringify(this.usuarios));
+    }
+  }
+
+  TarefasCategoria(categoria: string): Pessoa[] {
+    return this.usuarios.filter((usuario) => usuario.categoria === categoria);
+  }
+
+  transferirTarefa(event: DragEvent, usuario: Pessoa): void {
+    event.dataTransfer.setData('text/plain', usuario.tarefa);
+  }
 }
