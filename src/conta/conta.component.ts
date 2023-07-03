68e115f314e4cd44101e4f9b77deef0b535bfd60
ANA CLARA TOMASELLI BORCHARDT <ana_c_borchardt@estudante.sesisenai.org.br>
2023-07-03 14:03:51 -0300

    service

--- src/conta/conta.component.ts
+++ src/conta/conta.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/auth.service';
 import { user } from 'src/models/users/user';
 import { UserRepository } from 'src/repositories/user.respository';
 
